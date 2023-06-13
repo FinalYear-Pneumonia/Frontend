8f3d9e780f298c168d2d1a8bc709bfdf3e175262
E-turkson <108932582+E-turkson@users.noreply.github.com>
2023-06-13 04:07:29 +0000

    App.js file

---
+++ App.js
@@ -0,0 +1,40 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener('click', () =>{
+  container.classList.add("sign-up-mode");
+});
+
+
+sign_in_btn.addEventListener('click', () =>{
+  container.classList.remove("sign-up-mode");
+});
+
+
